4bf944ce4f2cd24a090cd50e7856d5c48adcc304
Valery Fremaux <valery.fremaux@gmail.com>
2012-09-12 14:32:26 +0200

    add new sections to use stats (download, user)

--- use_stats/settings.php
+++ use_stats/settings.php
@@ -9,7 +9,7 @@ $settings->add(new admin_setting_configtext('block_use_stats_threshold', get_str
                    get_string('configthreshold', 'block_use_stats'), '60'));
 
 $settings->add(new admin_setting_configtext('block_use_stats_capturemodules', get_string('capturemodules', 'block_use_stats'),
-                   get_string('configcapturemodules', 'block_use_stats'), 'course,forum,glossary,assignement,quiz,feedback,resource,lesson,survey'));
+                   get_string('configcapturemodules', 'block_use_stats'), 'course,download,user,forum,glossary,assignment,quiz,feedback,resource,lesson,survey'));
 
 $settings->add(new admin_setting_configtext('block_use_stats_ignoremodules', get_string('ignoremodules', 'block_use_stats'),
                    get_string('configignoremodules', 'block_use_stats'), ''));
