617bfbf9f5332a7779b5d1583a960111c720f960
valery <valery.fremaux@gmail.com>
2017-01-02 14:55:30 +0100

    Automated sync from SVN

--- version.php
+++ version.php
@@ -33,4 +33,4 @@ $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.8.0 (build 2016111100)';
 
 // Non moodle attributes.
-$plugin->codeincrement = '2.8.0000';
+$plugin->codeincrement = '2.8.0005';
