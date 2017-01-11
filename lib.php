5477b502057b8c2912ee6f8c2249d38ec4517907
valery <valery.fremaux@gmail.com>
2017-01-11 12:48:48 +0100

    Automated sync from SVN

--- lib.php
+++ lib.php
@@ -15,6 +15,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * Library for theme notifications.
+ *
  * @package   block_use_stats
  * @category  blocks
  * @copyright 2012 Wafa Adham,, Valery Fremaux
