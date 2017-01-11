8d6eb176065fb0f6de03ef875e719778162cda5f
valery <valery.fremaux@gmail.com>
2017-01-11 23:49:46 +0100

    Fix dhtmlxcalendar open event. Fix PostGreSQL query form (from Mudrak)

--- locallib.php
+++ locallib.php
@@ -109,7 +109,6 @@ function use_stats_extract_logs($from, $to, $for = null, $course = null) {
     // We search first enrol time still active for this user.
     $sql = "
         SELECT
-            ue.id,
             MIN(timestart) as timestart
         FROM
             {enrol} e,
@@ -1135,7 +1134,7 @@ function block_use_stats_get_log_range($userid, $from, $to) {
 function block_use_stats_get_sql_params() {
 
     $logmanager = get_log_manager();
-    $readers = $logmanager->get_readers('\core\log\sql_reader');
+    $readers = $logmanager->get_readers(use_stats_get_reader());
     $reader = reset($readers);
 
     if (empty($reader)) {
