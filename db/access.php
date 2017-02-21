b6a83db0903d5fe36363bf662ea6931254f2d27f
valery <valery.fremaux@gmail.com>
2017-02-21 23:51:41 +0100

    Add enable/disable of the end date in timerange selection mode / Fix end range to 23h59 of the current day

--- db/access.php
+++ db/access.php
@@ -96,7 +96,6 @@ $capabilities = array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
-            'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017021600;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017022100;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016051900;        // Requires this Moodle version.
 $plugin->component = 'block_use_stats'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.0 (build 2017021600)';
+$plugin->release = '3.1.0 (build 2017022100)';
 
 // Non moodle attributes.
 $plugin->codeincrement = '3.1.0007';
