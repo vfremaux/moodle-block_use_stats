bd740f6e09a644375ba6a0a03d967e69bc0ab5d2
valery <valery.fremaux@gmail.com>
2017-08-25 10:52:21 +0200

    code resync

--- locallib.php
+++ locallib.php
@@ -91,7 +91,7 @@ function use_stats_extract_logs($from, $to, $for = null, $course = null) {
                 list($insql, $inparams) = $DB->get_in_or_equal(array($course->id));
                 $courseenrolclause = "e.courseid $insql AND ";
             }
-        } else if (is_integer($course)) {
+        } else if (is_numeric($course)) {
             if (!empty($course)) {
                 $courseclause = " AND {$courseparm} = $course ";
                 list($insql, $inparams) = $DB->get_in_or_equal(array($course));
@@ -113,7 +113,7 @@ function use_stats_extract_logs($from, $to, $for = null, $course = null) {
                 list($insql, $inparams) = $DB->get_in_or_equal(array($course->id, 0, 1));
                 $courseenrolclause = "e.courseid $insql AND ";
             }
-        } else if (is_integer($course)) {
+        } else if (is_numeric($course)) {
             if (!empty($course)) {
                 $courseclause = " AND {$courseparm} IN ($course, 0, 1) ";
                 list($insql, $inparams) = $DB->get_in_or_equal(array($course, 0, 1));
@@ -278,6 +278,7 @@ function use_stats_aggregate_logs($logs, $dimension, $origintime = 0, $from = 0,
 
             // Let's get lap time to next log in track.
             $nexti = $i + 1;
+            $lognext = false;
             if (isset($logs[$i + 1])) {
                 /*
                  * Fetch ahead possible jumps over some non significant logs
@@ -298,7 +299,7 @@ function use_stats_aggregate_logs($logs, $dimension, $origintime = 0, $from = 0,
             if ($lap > $threshold) {
                 $lap = $lastpingcredit;
 
-                if (!block_use_stats_is_login_event($lognext->action)) {
+                if ($lognext && !block_use_stats_is_login_event($lognext->action)) {
                     $sessionpunch = true;
                 }
             }
@@ -353,7 +354,7 @@ function use_stats_aggregate_logs($logs, $dimension, $origintime = 0, $from = 0,
                 }
 
                 if ($continue) {
-                    if (block_use_stats_is_login_event(@$lognext->action)) {
+                    if ($lognext && block_use_stats_is_login_event(@$lognext->action)) {
                         // We are the last action before a new login.
                         @$aggregate['sessions'][$sessionid]->elapsed += $lap + $memlap;
                         @$aggregate['sessions'][$sessionid]->sessionend = $log->time + $lap + $memlap;
@@ -403,7 +404,7 @@ function use_stats_aggregate_logs($logs, $dimension, $origintime = 0, $from = 0,
                 // All other cases : login or non login.
                 if (block_use_stats_is_login_event($log->action)) {
                     // We are explicit login.
-                    if (!block_use_stats_is_login_event(@$lognext->action)) {
+                    if ($lognext && !block_use_stats_is_login_event($lognext->action)) {
                         if (!$preinit || $sessionid) {
                             // Not session 0, must increment.
                             if ($automatondebug || $backdebug) {
@@ -441,7 +442,7 @@ function use_stats_aggregate_logs($logs, $dimension, $origintime = 0, $from = 0,
                             $logbuffer .= " ... (P) session punch in : {$lognext->action} ";
                         }
                     }
-                    if ($sessionpunch || block_use_stats_is_login_event($lognext->action)) {
+                    if ($sessionpunch || !$lognext || block_use_stats_is_login_event($lognext->action)) {
                         // This record is the last one of the current session.
                         @$aggregate['sessions'][$sessionid]->sessionend = $log->time + $lap;
                         @$aggregate['sessions'][$sessionid]->elapsed += $lap;
@@ -1071,6 +1072,7 @@ function block_use_stats_render_aggregate(&$aggregate) {
         echo '<td width="20%">'.$usertotal->events.'</td>';
         echo '</tr>';
     }
+    echo '</table>';
 
     echo '<h3>Course total</h3>';
     echo '<table width="100%">';
@@ -1083,6 +1085,7 @@ function block_use_stats_render_aggregate(&$aggregate) {
         echo '<td width="20%">'.$coursetotal->events.'</td>';
         echo '</tr>';
     }
+    echo '</table>';
 
     echo '<h3>In course</h3>';
     echo '<table width="100%">';
@@ -1095,6 +1098,7 @@ function block_use_stats_render_aggregate(&$aggregate) {
         echo '<td width="20%">'.$coursetotal->events.'</td>';
         echo '</tr>';
     }
+    echo '</table>';
 
     echo '<h3>Activities</h3>';
     echo '<table width="100%">';
@@ -1107,6 +1111,7 @@ function block_use_stats_render_aggregate(&$aggregate) {
         echo '<td width="20%">'.$activitytotal->events.'</td>';
         echo '</tr>';
     }
+    echo '</table>';
 
     echo '<h3>Other</h3>';
     echo '<table width="100%">';
