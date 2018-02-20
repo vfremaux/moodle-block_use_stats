107ea96bc2c6cf362189e0fa8da8d09613caa212
valery <valery.fremaux@gmail.com>
2018-02-20 22:38:52 +0100

    Automated sync from SVN

--- db/services.php
+++ db/services.php
@@ -21,6 +21,7 @@
  * @copyright  2017 Valery Fremaux (valery.fremaux@gmail.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 
 $functions = array(
 
