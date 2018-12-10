11017c7b54dc87d75245a762d50a263686e99f84
valery <valery.fremaux@gmail.com>
2018-12-11 00:59:41 +0100

    Moodle 36 init

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018032400;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018042700;        // Requires this Moodle version.
+$plugin->requires  = 2018112800;        // Requires this Moodle version.
 $plugin->component = 'block_use_stats'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.5.0 (build 2018032400)';
+$plugin->release = '3.6.0 (build 2018032400)';
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.5.0010';
+$plugin->codeincrement = '3.6.0010';
 $plugin->privacy = 'dualrelease';
