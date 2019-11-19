2bab771665f93b93a85e0b36e036c4014f152f4a
valery <valery.fremaux@gmail.com>
2019-11-19 10:06:09 +0100

    Automated sync from SVN

--- lang/fr/block_use_stats.php
+++ lang/fr/block_use_stats.php
@@ -111,10 +111,12 @@ $string['use_stats_name'] = 'Acces distant aux statistiques d\'usage'; // Is a @
 $string['use_stats_rpc_service'] = 'Lecture distante des statistiques'; // Is a @DYNAKEY.
 $string['use_stats_rpc_service_name'] = 'Accès distant aux statistiques d\'usage'; // Is a @DYNAKEY.
 $string['youspent'] = 'Cumul&nbsp;:&ensp;';
+$string['warningusestateenrolfilter'] = 'La vérification des dates d\'inscription est activée dans le bloc "Mesure d\'activité". Ceci peut avoir un effet de filtrage innatendu pour des utiisateurs ayant eu des
+activités AVANT la date d\'inscription la plus récente constatée dans le cours.';
 
 $string['plugindist_desc'] = '<p>Ce plugin est distribué dans la communauté Moodle pour l\'évaluation de ses fonctions centrales
 correspondant à une utilisation courante du plugin. Une version "professionnelle" de ce plugn existe et est distribuée
 sous certaines conditions, afin de soutenir l\'effort de développement, amélioration; documentation et suivi des versions.</p>
 <p>Contactez un distributeur pour obtenir la version "Pro" et son support.</p>
 <ul><li><a href="http://www.activeprolearn.com/plugin.php?plugin=block_use_stats&lang=fr">ActiveProLearn SAS</a></li>
-<li><a href="http://www.edunao.com">Edunao SAS</a></li></ul>';
+</ul>';
--- test/test_client.php
+++ test/test_client.php
@@ -63,6 +63,28 @@ class test_client {
         return $this->send($serviceurl, $params);
     }
 
+    public function test_get_users_course_stats($uidsource = '', $uids = array(), $cidsource = '', $cid = 0, $from = 0, $to = 0, $score = true) {
+
+        if (empty($this->t->wstoken)) {
+            echo "No token to proceed\n";
+            return;
+        }
+
+        $params = array('wstoken' => $this->t->wstoken,
+                        'wsfunction' => 'block_use_stats_get_users_course_stats',
+                        'moodlewsrestformat' => 'json',
+                        'uidsource' => $uidsource,
+                        'uids' => $uids,
+                        'cidsource' => $cidsource,
+                        'cid' => $cid,
+                        'from' => $from,
+                        'to' => $to,
+                        'score' => $score);
+
+        $serviceurl = $this->t->baseurl.$this->t->service;
+
+        return $this->send($serviceurl, $params);
+    }
 
     protected function send($serviceurl, $params) {
         $ch = curl_init($serviceurl);
@@ -101,8 +123,14 @@ $client->test_get_user_stats('idnumber', 'ADMIN', 'id', 2); // Test one course o
 $client->test_get_user_stats('username', 'admin', 'id', 2); // Test one course one users.
 $client->test_get_user_stats('id', 3, 'id', 2); // Test one course one users.
 */
-$client->test_get_users_stats('id', array(3,4,5), 'id', 2); // Test one course one users.
-$client->test_get_users_stats('idnumber', array('ADMIN','ID_A1','ID_A2'), 'id', 2); // Test one course one users.
+
+/*
+$client->test_get_users_stats('id', array(3,4,5), 'id', 2); // Test one course some users.
+$client->test_get_users_stats('idnumber', array('ADMIN','ID_A1','ID_A2'), 'id', 2); // Test one course some users by idnumber.
+*/
+
+$client->test_get_users_course_stats('id', array(3,4,5), 'id', 2); // Test one course some users.
+$client->test_get_users_course_stats('idnumber', array('ADMIN','ID_A1','ID_A2'), 'id', 2); // Test one course some users by idnumber.
 
 /*
 $client->test_get_user_stats('id', 3, 'id', 2, 1489095902, 1489195902, 1); // Test time range with score.
