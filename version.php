2debe4533a5a5a8f0970ddd0620b90b0f2f1ab26
Valery Fremaux <valery.fremaux@gmail.com>
2023-06-05 09:45:13 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -34,5 +34,5 @@ $plugin->supported = [39,311];
 $plugin->release = '3.9.0 (build 2023060400)';
 
 // Non Moodle attributes.
-$plugin->codeincrement = '3.9.0021';
+$plugin->codeincrement = '3.9.0022';
 $plugin->privacy = 'dualrelease';
