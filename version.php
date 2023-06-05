16f6cf9f345203ac0b241c409fbf71e9e34de34c
Valery Fremaux <valery.fremaux@gmail.com>
2023-06-05 18:39:55 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -26,11 +26,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021101100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023060500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2022112801;        // Requires this Moodle version.
 $plugin->component = 'block_use_stats'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.1.0 (build 2021101100)';
+$plugin->release = '4.1.0 (build 2023060500)';
 $plugin->supported = [401,402];
 
 // Non Moodle attributes.
